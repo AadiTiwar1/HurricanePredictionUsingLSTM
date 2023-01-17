882f490a4dde4bb39a5a0c09a4fa5c745efec7cb
Aadi Tiwari <110158904+AadiTiwar1@users.noreply.github.com>
2023-01-16 17:39:46 -0800

    Update train_model.py

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -13,29 +13,6 @@ from torch import nn
 
 df = pd.read_csv('../static/dataset/atlantic.csv')
 
-df.drop(['status_of_system', 'Unnamed: 6', 'Unnamed: 7', 'Unnamed: 8','Unnamed: 9', 
-        'Unnamed: 10', 'Unnamed: 11','Unnamed: 12', 'Unnamed: 13', 'Unnamed: 14', 
-        'Unnamed: 15', 'Unnamed: 16', 'Unnamed: 17', 'Unnamed: 18'], inplace=True, axis=1)
-df['date'] = pd.to_datetime(df['date'], format = "%Y%m%d").dt.strftime('%Y-%m-%d')
-
-
-df['next_windspeed'] =  df['max_sustained_wind'].shift(-1)
-
-# df.to_csv('cleanedHurricaneData.csv', index=False)
-
-df['date'] = df['date'].apply(lambda x: float(x.split()[0].replace('-', '')))
-df['latitude'] = df['latitude'].map(lambda x: float(x.rstrip('NEWS')))
-df['longitude'] = df['longitude'].map(lambda x: float(x.rstrip('NEWS')))
-df['max_sustained_wind'] = df['max_sustained_wind'].replace(-99, np.nan)
-df['next_windspeed'] = df['next_windspeed'].replace(-99, np.nan)
-
-df['max_sustained_wind'] =  df['max_sustained_wind'].fillna(method='ffill', limit=1000)
-df['max_sustained_wind'] =  df['max_sustained_wind'].fillna(method='bfill', limit=1000)
-df['central_pressure'] = df['central_pressure'].fillna(method='ffill', limit=1000)
-df['central_pressure'] = df['central_pressure'].fillna(method='bfill', limit=1000)
-df['next_windspeed'] = df['next_windspeed'].fillna(method='ffill', limit=1000)
-df['next_windspeed'] = df['next_windspeed'].fillna(method='bfill', limit=1000)
-
 target = "next_windspeed"
 features = list(df.columns.difference([target]))
 size = int(len(df) * 0.8)
