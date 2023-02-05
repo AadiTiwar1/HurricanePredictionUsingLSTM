ab39cd4eb002e6ba82d6d7e1e177d98a7f409a13
Arnav38025 <71982906+Arnav38025@users.noreply.github.com>
2023-02-04 21:53:14 -0800

    Update download_data.py

--- scripts/download_data.py
+++ scripts/download_data.py
@@ -3,4 +3,4 @@ req = requests.get("https://query.data.world/s/gwuvcaposio5kejfu322u3znsrz637")
 url_content = req.content
 
 with open('static/dataset/atlantic.csv', 'wb') as csv_file:
-    csv_file.write(url_content)
+    csv_file.write(url_content) 
